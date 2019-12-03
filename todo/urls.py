066701829c400e508d829826ddf662b305e9a01f
wata-pom <w.kumagai1224@gmail.com>
2019-12-04 00:53:13 +0900

    DetailViewの作成
     データベースのobjectをpk(primary_key)の指定によって呼び出す

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from todo.views import TodoList
+from todo.views import TodoList, TodoDetail
 
 urlpatterns = [
     path('list/', TodoList.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
+from .models import TodoModel
 
 
 class TodoList(ListView):
     template_name = 'list.html'
+    model = TodoModel
+
+
+class TodoDetail(DetailView):
+    template_name = 'Detail.html'
+    model = TodoModel
